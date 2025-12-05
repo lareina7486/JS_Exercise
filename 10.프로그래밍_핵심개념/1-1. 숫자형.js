17eaca0210305056c10444df735fdd1a66e3701f
lareina7486 <lareina7486@gmail.com>
2025-12-05 14:22:16 +0900

    chore: JavaScript number type practice

---
+++ 10.프로그래밍_핵심개념/1-1. 숫자형.js
@@ -0,0 +1,11 @@
+console.log(25000 + 3000); // 덧셈
+console.log(50000 - 25000); // 뺄셈
+console.log(25000 * 3); // 곱셈
+console.log(25000 / 5); // 나눗셈
+
+console.log(28 % 6); // 나머지 연산
+const num1 = 2**10; // 거듭제곱 연산
+
+console.log(num1);
+console.log(num2);
+
