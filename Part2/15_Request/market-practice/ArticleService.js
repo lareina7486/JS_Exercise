8672198b3cbba6361ca59daa429fd0d52888f0c2
lareina7486 <lareina7486@gmail.com>
2026-01-19 23:36:14 +0900

    fix: 게시글 API CRUD 수정

--- Part2/15_Request/market-practice/ArticleService.js
+++ Part2/15_Request/market-practice/ArticleService.js
@@ -1,13 +1,22 @@
 const BASE_URL = 'https://panda-market-api-crud.vercel.app';
 
 // 공통 응답 처리
-function handleResponse(response) {
-  if (!response.ok) {
-    const err = new Error(`Article API Error: ${response.status} ${response.statusText}`);
-    err.status = res.status;
-    throw err;
-  }
-  return response;
+function request(url, options = {}) {
+  return fetch(url, options)
+    .then(res => {
+      if (!res.ok) {
+        const err = new Error(`API Error: ${res.status} ${res.statusText}`);
+        err.status = res.status;
+        throw err;
+      }
+      
+      if (res.status === 204) return null;
+      return res.json();
+    })
+    .catch(err => {
+      console.error(`API Call Failed (${options.method || 'GET'} ${url}):`, err);
+      throw err;
+    });
 }
 
 // 게시글 목록 조회
@@ -17,58 +26,53 @@ export function getArticleList({ page = 1, pageSize = 10, keyword = '' } = {}) {
   url.searchParams.set('pageSize', pageSize);
   if (keyword) url.searchParams.set('keyword', keyword);
 
-  return fetch(url, { method: 'GET' })
-    .then(handleResponse)
-    .then(res => res.json())
-    .catch(err => {
-      console.error('getArticleList Error:', err);
-    });
+  return request(url.toString());
+
+  // return fetch(url, { method: 'GET' })
+  //   .then(handleResponse)
+  //   .then(res => res.json())
+  //   .catch(err => {
+  //     console.error('getArticleList Error:', err);
+  //     throw err;
+  //   });
 }
 
 // 게시글 단일 조회
 export function getArticle(articleId) {
-  return fetch(`${BASE_URL}/articles/${articleId}`, { method: 'GET' })
-    .then(handleResponse)
-    .then(res => res.json())
-    .catch(err => {
-      console.error('getArticle Error:', err);
-    });
+  return request(`${BASE_URL}/articles/${articleId}`);
 }
 
 // 게시글 생성
 export function createArticle({ title, content, image }) {
-  return fetch(`${BASE_URL}/articles`, {
+  return request(`${BASE_URL}/articles`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ title, content, image }),
-  })
-    .then(handleResponse)
-    .then(res => res.json())
-    .catch(err => {
-      console.error('createArticle Error:', err);
-    });
+    body: JSON.stringify(payload),
+  });
+    // .then(handleResponse)
+    // .then(res => res.json())
+    // .catch(err => {
+    //   console.error('createArticle Error:', err);
+    //   throw err;
+    // });
 }
 
 // 게시글 수정
 export function patchArticle(articleId, payload) {
-  return fetch(`${BASE_URL}/articles/${articleId}`, {
+  return request(`${BASE_URL}/articles/${articleId}`, {
     method: 'PATCH',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(payload),
-  })
-    .then(handleResponse)
-    .then(res => res.json())
-    .catch(err => {
-      console.error('patchArticle Error:', err);
-    });
+  });
+    // .then(handleResponse)
+    // .then(res => res.json())
+    // .catch(err => {
+    //   console.error('patchArticle Error:', err);
+    //   throw err;
+    // });
 }
 
 // 게시글 삭제
 export function deleteArticle(articleId) {
-  return fetch(`${BASE_URL}/articles/${articleId}`, { method: 'DELETE' })
-    .then(handleResponse)
-    .then(res => res.json())
-    .catch(err => {
-      console.error('deleteArticle Error:', err);
-    });
+    return request(`${BASE_URL}/articles/${articleId}`, { method: 'DELETE' });
 }
--- Part2/SPRINT3.md
+++ Part2/SPRINT3.md
@@ -20,11 +20,10 @@ https://panda-market-api-crud.vercel.app/docs/
 [ ] request body에 title, content, image 를 포함해 주세요.
 [ ] patchArticle() : PATCH 메서드를 사용해 주세요.
 [ ] deleteArticle() : DELETE 메서드를 사용해 주세요.
-[ ] fetch 혹은 axios 를 이용해 주세요.
 
+[ ] fetch 혹은 axios 를 이용해 주세요.
 [ ] 응답의 상태 코드가 2XX가 아닐 경우, 에러메시지를 콘솔에 출력해 주세요.
 [ ] .then() 메서드를 이용하여 비동기 처리를 해주세요.
-
 [ ] .catch() 를 이용하여 오류 처리를 해주세요.
 
 ---
@@ -41,8 +40,8 @@ https://panda-market-api-crud.vercel.app/docs/
 
 [ ] patchProduct() : PATCH 메서드를 사용해 주세요.
 [ ] deleteProduct() : DELETE 메서드를 사용해 주세요.
-[ ] async/await 을 이용하여 비동기 처리를 해주세요.
 
+[ ] async/await 을 이용하여 비동기 처리를 해주세요.
 [ ]  try/catch 를 이용하여 오류 처리를 해주세요.
 
 [√]  구현한 함수들을 아래와 같이 파일을 분리해 주세요.
