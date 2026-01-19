05b94c2ee8a6d21f88ec51703a34585a0259ce55
lareina7486 <lareina7486@gmail.com>
2026-01-19 23:38:06 +0900

    feat: 게시글, 상품 API 테스트 코드 작성

--- Part2/15_Request/market-practice/main.js
+++ Part2/15_Request/market-practice/main.js
@@ -14,6 +14,36 @@ import {
   deleteProduct,
 } from './ProductService.js';
 
-// Article test
+// Article 테스트
+getArticleList({ page: 1, pageSize: 5 })
+  .then(data => {
+    console.log('Article List:', data);
+    const firstId = data.list[0].id;
+    return getArticle(firstId);
+  })
+  .then(detail => console.log('Article Detail:', detail));
 
-// Product test
+// getArticleList({ page: 1, pageSize: 5 })
+//   .then(data => console.log('Article List:', data));
+
+// getArticle(1)
+//   .then(data => console.log('Article Detail:', data));
+
+// Product 테스트
+(async () => {
+  const products = await getProductList({ page: 1, pageSize: 5 });
+  console.log('Product List:', products);
+
+  const firstProductId = products.list[0].id;
+  const product = await getProduct(firstProductId);
+  console.log('Product Detail:', product);
+})();
+
+
+// (async () => {
+//   const products = await getProductList({ page: 1, pageSize: 5 });
+//   console.log('Product List:', products);
+
+//   const product = await getProduct(1);
+//   console.log('Product Detail:', product);
+// })();
