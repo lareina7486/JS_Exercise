ed5e0b37776c756fe4dfe5bfe21419fbe15bf96b
lareina7486 <lareina7486@gmail.com>
2026-02-15 23:47:42 +0900

    feat: 연산자에 대한 예제 추가

---
+++ Js_basic/1_basics/7_operators.js
@@ -0,0 +1,18 @@
+// Operator (연산자)
+
+// 산술 연산자: 덧셈, 뺄셈, 곱셈, 나눗셈, 나머지
+console.log(10 + 10); // 20
+console.log(10 - 10); // 0
+console.log(10 * 10); // 100
+console.log(10 / 10); // 1
+console.log(10 % 10); // 0
+
+console.log(10 * (10 + 10));
+
+// 증가와 감소
+let number = 1;
+number++;
+console.log(number); // 2
+
+number--;
+console.log(number); // 1
